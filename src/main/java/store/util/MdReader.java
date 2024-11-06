2ea7520216ff42e9c0514673df9ff99df331632f
pokm321 <pokm90@gmail.com>
2024-11-06 17:55:30 +0900

    feat: add a feature to read md files

---
+++ src/main/java/store/util/MdReader.java
@@ -0,0 +1,40 @@
+package store.util;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.Buffer;
+
+public class MdReader {
+    BufferedReader reader;
+
+    public MdReader(String path){
+        try {
+            reader = new BufferedReader(new FileReader(path));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void readLines() {
+        try {
+            String line;
+            while ((line = readLine()) != null) {
+                System.out.println(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String readLine(){
+        try {
+            return reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
