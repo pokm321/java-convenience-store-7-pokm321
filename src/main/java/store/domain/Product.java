deb92f6468aa6af46ae2d8ac2fa2a8368a95ad13
pokm321 <pokm90@gmail.com>
2024-11-06 17:15:29 +0900

    feat: add Product class for containing all the products' information

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -1,4 +1,32 @@
 package store.domain;
 
 public class Product {
+
+    private final String name;
+    private final int price;
+    private final int quantity;
+    private final String promotion;
+
+    public Product(String name, int price, int quantity, String promotion) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.promotion = promotion;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getPromotion() {
+        return promotion;
+    }
 }
--- src/test/java/store/domain/ProductTest.java
+++ src/test/java/store/domain/ProductTest.java
@@ -1,4 +1,18 @@
 package store.domain;
 
-public class Product {
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class ProductTest {
+
+    @Test
+    void 객체_생성_테스트() {
+        Product coke = new Product("콜라", 1000, 10, "반짝할인");
+
+        assertThat(coke.getName()).isEqualTo("콜라");
+        assertThat(coke.getPrice()).isEqualTo(1000);
+        assertThat(coke.getQuantity()).isEqualTo(10);
+        assertThat(coke.getPromotion()).isEqualTo("반짝할인");
+    }
 }
