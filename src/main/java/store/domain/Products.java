75f357862a014ce93de536896c1f6c2b244f64a8
pokm321 <pokm90@gmail.com>
2024-11-10 22:23:02 +0900

    fix: make the stock print the nonexisting non-promotion products

--- src/main/java/store/domain/Products.java
+++ src/main/java/store/domain/Products.java
@@ -8,6 +8,10 @@ import store.view.ViewErrors;
 
 public class Products {
 
+    private static final int INDEX_NAME = 0;
+    private static final int INDEX_PRICE = 1;
+    private static final int INDEX_QUANTITY = 2;
+    private static final int INDEX_PROMOTION = 3;
     private static final int FIELD_COUNT = 4;
     private static final String DELIMITER = ",";
 
@@ -17,12 +21,51 @@ public class Products {
         validate(line);
 
         List<String> fields = getFields(line);
+
+        if (tryMergingExistingProduct(fields)) {
+            return;
+        }
+
+        addNewProduct(fields);
+    }
+
+    private boolean tryMergingExistingProduct(List<String> fields) {
+        if (!getProductsByFields(fields).isEmpty()) {
+            Product product = getProductsByFields(fields).getFirst();
+            product.setQuantity(product.getQuantity() + Integer.parseInt(fields.get(INDEX_QUANTITY)));
+            return true;
+        }
+        return false;
+    }
+
+    private List<Product> getProductsByFields(List<String> fields) {
+        return listOfProducts.stream()
+                .filter(product -> product.getName().equals(fields.get(INDEX_NAME)))
+                .filter(product -> product.getPromotion().equals(fields.get(INDEX_PROMOTION))).toList();
+    }
+
+    private void addNewProduct(List<String> fields) {
         listOfProducts.add(new Product(
-                fields.get(0),
-                Long.parseLong(fields.get(1)),
-                Integer.parseInt(fields.get(2)),
-                fields.get(3)
-        ));
+                fields.get(INDEX_NAME),
+                Long.parseLong(fields.get(INDEX_PRICE)),
+                Integer.parseInt(fields.get(INDEX_QUANTITY)),
+                fields.get(INDEX_PROMOTION)));
+
+        if (isPromotionedProduct(fields)) {
+            addNormalProductWithZeroQuantity(fields);
+        }
+    }
+
+    private boolean isPromotionedProduct(List<String> fields) {
+        return !fields.get(INDEX_PROMOTION).equals(MdKeywords.NULL.getValue());
+    }
+
+    private void addNormalProductWithZeroQuantity(List<String> fields) {
+        listOfProducts.add(new Product(
+                fields.get(INDEX_NAME),
+                Long.parseLong(fields.get(INDEX_PRICE)),
+                0,
+                MdKeywords.NULL.getValue()));
     }
 
     private void validate(String line) {
@@ -58,8 +101,8 @@ public class Products {
 
     private void validateInteger(List<String> fields) {
         try {
-            Long.parseLong(fields.get(1));
-            Integer.parseInt(fields.get(2));
+            Long.parseLong(fields.get(INDEX_PRICE));
+            Integer.parseInt(fields.get(INDEX_QUANTITY));
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(MdErrors.MD_INTEGER_ERROR.getMessage());
         }
